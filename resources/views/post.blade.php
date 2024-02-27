eee0e624a39a53055257b3ddd419d4aa11f0c745
Nazrul Farhan <farhan.nazrul99@gmail.com>
2024-02-28 00:26:49 +0800

    saya yakin saya slay

---
+++ resources/views/post.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+    <h1><a href="/post">Hello World</a></h1>
+
+    <p>
+        Paan hensem siot kenapa dia hensem sangat penad la camni
+    </p>
+    </article>
+
+    <a href="/">Go Back</a>
+
+</body>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+    <h1><a href="/post">Hello World</a></h1>
+
+    <p>
+        Paan hensem siot kenapa dia hensem sangat penad la camni
+    </p>
+    </article>
+
+    <article>
+        <h1><a href="/post">Hello World</a></h1>
+    
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam dignissimos illum, vel voluptates modi ab maxime mollitia accusantium rerum fuga sed et! Magni quibusdam consequuntur maxime atque iste laudantium sequi.
+        </p>
+        </article>
+
+        <article>
+            <h1><a href="/post">Hello World</a></h1>
+        
+            <p>
+                ini karangan
+            </p>
+            </article>
+
+</body>
